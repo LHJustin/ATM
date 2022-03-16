49316f86838bae203a126f380f5b55b5bc131b91
Justin <masterofleaf@gmail.com>
2022-03-16 18:09:52 +0800

    forth commit show chatroom

--- app/src/main/java/com/tom/atm/SecondFragment.kt
+++ app/src/main/java/com/tom/atm/SecondFragment.kt
@@ -1,21 +1,27 @@
 package com.tom.atm
 
+import android.media.Image
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.google.gson.Gson
 import com.tom.atm.databinding.FragmentSecondBinding
 import com.tom.atm.databinding.RowChatroomBinding
 import okhttp3.*
 import okio.ByteString
+import java.net.URL
 import java.util.concurrent.TimeUnit
+import kotlin.concurrent.thread
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -27,6 +33,8 @@ class SecondFragment : Fragment() {
         ChatRoom("101103","Miau3", "Welcome3"),
         ChatRoom("101104","Miau4", "Welcome4")
     )
+    //test chatroom list裡的變數
+    var rooms = mutableListOf<Lightyear>()
     private val TAG = MainActivity ::class.java.simpleName
     private var _binding: FragmentSecondBinding? = null
     lateinit var websocket : WebSocket
@@ -96,15 +104,34 @@ class SecondFragment : Fragment() {
             val message = binding.edMessage.text.toString()
             val json = "{\"action\":\"N\", \"content\": \"$message\"}"
 //            websocket.send(json)
-            val j = Gson().toJson(Message("N", message))
+            val j = Gson().toJson(Messagej("N", message))
             websocket.send(j)
         }
         //Recycler's Adapter
-        binding.recycler.hasFixedSize()
-        binding.recycler.layoutManager = LinearLayoutManager(requireContext())
-        binding.recycler.adapter = ChatRoomAdapter()
+        binding.recycler.setHasFixedSize(true)
+        binding.recycler.layoutManager = GridLayoutManager(requireContext(),2)
+        var adapter = ChatRoomAdapter()
+        binding.recycler.adapter = adapter
+        //test message
+        thread {
+            val json = URL("https://api.jsonserve.com/hQAtNk").readText()
+            val msg = Gson().fromJson(json, Message::class.java)
+            Log.d(TAG, "msg: ${msg.body.text}")
 
-
+        }
+        //test chatroom list
+        thread {
+            val json = URL("https://api.jsonserve.com/qHsaqy").readText()
+            val chatRooms = Gson().fromJson(json, Chatrooms::class.java)
+            Log.d(TAG, "rooms: ${chatRooms.result.lightyear_list.size}")
+            //fill list with new coming data
+            rooms.clear()
+            rooms.addAll(chatRooms.result.lightyear_list)
+            //List<LightYear>
+            activity?.runOnUiThread{
+                adapter.notifyDataSetChanged()
+            }
+        }
     }
 
     inner class ChatRoomAdapter : RecyclerView.Adapter<BindingViewHolder>() {
@@ -117,13 +144,20 @@ class SecondFragment : Fragment() {
         }
 
         override fun onBindViewHolder(holder: BindingViewHolder, position: Int) {
-            val room = chatRooms[position]
-            holder.host.setText(room.hostname)
-            holder.title.setText(room.title)
+            val lightyear = rooms[position]
+            holder.host.setText(lightyear.stream_title)
+            holder.title.setText(lightyear.nickname)
+            holder.tag.setText(lightyear.tags)
+            Glide.with(this@SecondFragment).load(lightyear.head_photo)
+                .into(holder.headShot)
+            //itemView包括整個
+            holder.itemView.setOnClickListener {
+                chatRoomClicked(lightyear)
+            }
         }
 
         override fun getItemCount(): Int {
-            return chatRooms.size
+            return rooms.size
         }
 
     }
@@ -131,17 +165,26 @@ class SecondFragment : Fragment() {
     inner class  ChatRoomViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val host = view.findViewById<TextView>(R.id.chatroom_host_name)
         val title = view.findViewById<TextView>(R.id.chatroom_title)
+        val tag = view.findViewById<TextView>(R.id.chatroom_tag)
+        val headShot = view.findViewById<ImageView>(R.id.head_shot)
     }
 
     inner class  BindingViewHolder(val binding: RowChatroomBinding): RecyclerView.ViewHolder(binding.root) {
         val host = binding.chatroomHostName
         val title = binding.chatroomTitle
+        val headShot = binding.headShot
+        val tag = binding.chatroomTag
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-}
 
-data class Message(val action:String, val content:String)
+    //SecondFragment
+    fun chatRoomClicked(lightyear: Lightyear){
+
+    }
+}
+//ChatClasses會用到先註解
+data class Messagej(val action:String, val content:String)
